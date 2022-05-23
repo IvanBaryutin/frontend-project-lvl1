0e820bf873365be251e5cb780c7f60f091ea74ec
Ivan Baryutin <vanessru@yandex.ru>
2022-05-23 14:59:23 +0300

    Add: CodeClimate + Eslint + Github Actions settings

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export const sayHello = () => {
-    const userName = readlineSync.question('Your answer: ');
-    console.log(`Hello ${userName}!`);
-}
+  const userName = readlineSync.question('Your answer: ');
+  console.log(`Hello ${userName}!`);
+};
+
+export default sayHello;
