6713bc55d8b1cc5aee250cd7595f5494f9e06260
Ivan Baryutin <vaness97@gmail.com>
2022-05-23 23:06:31 +0300

    Add: isEvenGame function

--- src/isEvenGame.js
+++ src/isEvenGame.js
@@ -1,9 +1,30 @@
 import readlineSync from 'readline-sync';
 
 export const isEven = () => {
-  const randomNumber = Math.floor(Math.random() * 100);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  let randomNumber = 0;
+  let answer = '';
+  let correctAnswer = '';
+  let winner = true;
+
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log(randomNumber);
+  for (let counter = 0; counter <= 2; counter += 1) {
+    randomNumber = Math.floor(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
+    answer = readlineSync.question('Your answer: ');
+    correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.)`);
+      winner = false;
+      break;
+    }
+  }
+  if (winner === true) {
+    console.log(`Congratulations, ${userName}!`);
+  }
 };
 
 export default isEven;
