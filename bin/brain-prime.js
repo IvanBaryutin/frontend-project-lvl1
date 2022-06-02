a3842c3abf40946aebbe878662973b843a45fbc4
Ivan Baryutin <vanessru@yandex.ru>
2022-06-02 15:58:48 +0300

    Add: перепишем модуль игры prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { isPrime } from '../src/games/isPrimeGame.js';
+import brainPrimeGame from '../src/games/isPrimeGame.js';
 
-isPrime();
+brainPrimeGame();
--- src/games/isPrimeGame.js
+++ src/games/isPrimeGame.js
@@ -1,43 +1,29 @@
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
 
-export const isPrime = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  let randomNumber = 0;
-  let answer = '';
-  let correctAnswer = '';
-  let winner = true;
+import playGame from '../index.js';
 
-  const isPrimeNumber = (num) => {
-    let res;
-    for (let i = 2; i < num; i += 1) {
-      if (num % i === 0) {
-        res = false;
-        break;
-      }
-      res = true;
-    }
-    return res;
-  };
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let counter = 0; counter <= 2; counter += 1) {
-    randomNumber = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomNumber}`);
-    answer = readlineSync.question('Your answer: ');
-    correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.)`);
-      winner = false;
+// Функция проверки числа на простое число
+const isPrimeNumber = (num) => {
+  let res;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      res = false;
       break;
     }
+    res = true;
   }
-  if (winner === true) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  return res;
 };
 
-export default isPrime;
+export const isPrime = () => {
+  const randomNumber = Math.floor(Math.random() * 100);
+  const correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
+
+  return [randomNumber, correctAnswer];
+};
+
+const brainPrimeGame = () => playGame(description, isPrime);
+
+export default brainPrimeGame;
