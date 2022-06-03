93a9a9c0ccaea6ba70838c9beb8a448030815f62
Ivan Baryutin <vanessru@yandex.ru>
2022-06-03 17:28:22 +0300

    Fix: fix isPrime function

--- src/games/isEvenGame.js
+++ src/games/isEvenGame.js
@@ -4,13 +4,13 @@ import playGame from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = () => {
+const genEvenGame = () => {
   const randomNumber = Math.floor(Math.random() * 100);
   const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
 
   return [randomNumber, correctAnswer];
 };
 
-const startGame = () => playGame(description, isEven);
+const startGame = () => playGame(description, genEvenGame);
 
 export default startGame;
