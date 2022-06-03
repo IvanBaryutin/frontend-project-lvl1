d22d6515e0fbcb409aaeec11003e4ed629c4b2de
Ivan Baryutin <vanessru@yandex.ru>
2022-06-03 16:50:29 +0300

    Fix: fix variables naming

--- src/games/isPrimeGame.js
+++ src/games/isPrimeGame.js
@@ -5,7 +5,7 @@ import playGame from '../index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 // Функция проверки числа на простое число
-const isPrimeNumber = (num) => {
+const isPrime = (num) => {
   let res = true;
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
@@ -18,7 +18,7 @@ const isPrimeNumber = (num) => {
 
 export const genPrimeGame = () => {
   const randomNumber = Math.floor(Math.random() * 100);
-  const correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
 
   return [randomNumber, correctAnswer];
 };
